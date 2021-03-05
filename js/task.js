8c92a42acdcc31e7b32c3db56dd7ab188d0f9277
Andrii Dorohov <wedpositive@gmail.com>
2021-03-05 20:36:25 +0200

    1

--- js/task.js
+++ js/task.js
@@ -74,7 +74,7 @@ console.log(calculateTotalBalance(users)); // 20916
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
 const getUsersWithFriend = (users, friendName) => {
-    return users.filter(({friends}) => friends.includes(friendName));
+    return users.filter(({friends}) => friends.includes(friendName)).map(user => user.name);;
 };
 
 console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
@@ -83,13 +83,16 @@ console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sher
 // Задание 9
 // Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 
+
 const getNamesSortedByFriendsCount = users => {
-    return users.sort((a, b) => a.friends.length - b.friends.length).map(user => user.name);
+    return users.slice().sort((a, b) => a.friends.length - b.friends.length).map(user => user.name);
     
 };
 
+
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
 
 // Задание 10
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
